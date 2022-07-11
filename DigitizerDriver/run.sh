65268485212284b48c1cb9c63362b0d772399e55
WenZhan Song <wsong@uga.edu>
2022-07-10 20:38:40 -0400

    Update run.sh

--- DigitizerDriver/run.sh
+++ DigitizerDriver/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 now=$(date +"%T")
 SERVICE="python3"
+ADCDRIVER="/home/pi/NewDot/DigitizerDriver"
 if pgrep -x "$SERVICE" >/dev/null
 then
-    echo "$SERVICE is running at $now" > /home/pi/NewDot/log.txt
+    echo "$SERVICE is running at $now" > $ADCDRIVER/log.txt
 else
-    echo "$SERVICE stopped at $now" > /home/pi/NewDot/log.txt
-    nohup sudo /usr/bin/python3 /home/pi/NewDot/serialClient_final.py /dev/ttyS0 > /home/pi/NewDot/log.txt &
+    echo "$SERVICE stopped at $now" > $ADCDRIVER/log.txt
+    nohup sudo /usr/bin/python3 $ADCDRIVER/serialClient_final.py /dev/ttyS0 > $ADCDRIVER/log.txt &
 fi
